ea2a7818fcfeb3469bb1b0f1bb991f0696f9b901
Carmen Geelhaar <camina.apps@gmail.com>
2023-09-06 18:50:13 +0200

    add markdown preview

--- LazyVim/lua/plugins/editor.lua
+++ LazyVim/lua/plugins/editor.lua
@@ -19,4 +19,12 @@ return {
       },
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
