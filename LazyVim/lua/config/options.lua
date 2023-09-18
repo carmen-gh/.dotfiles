6001120195bd96309c1b9e0a1371220b0efea4f9
camina <camina.apps@gmail.com>
2023-09-18 09:25:19 +0200

    neovim conceallevel and spell

--- LazyVim/lua/config/options.lua
+++ LazyVim/lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-vim.opt.conceallevel = 0 -- do not hide " in json files
 
 vim.opt.numberwidth = 4
 vim.opt.statuscolumn = "%= %{&nu?(&rnu && v:relnum ? v:relnum : v:lnum) : ''}%=%s"
