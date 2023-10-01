73890dd3689a0e951c8b93fa314f2fa067ca8c08
Carmen Geelhaar <camina.apps@gmail.com>
2023-10-01 21:16:00 +0200

    update lazyvim config

--- LazyVim/lua/config/keymaps.lua
+++ LazyVim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jj", "<ESC>") -- better escape
+vim.keymap.set("i", "jj", "<ESC>", { noremap = false }) -- better escape
 vim.keymap.set("n", "x", '"_x') -- delete single character without copying into register
 vim.keymap.set("v", "<leader>p", [["_dP]]) -- paste and keep
 vim.keymap.set("n", "<leader>d", [["_d]]) -- delete without putting into register
@@ -13,8 +13,3 @@ vim.keymap.set("n", "<UP>", "<NOP>")
 vim.keymap.set("n", "<DOWN>", "<NOP>")
 vim.keymap.set("n", "<LEFT>", "<NOP>")
 vim.keymap.set("n", "<RIGHT>", "<NOP>")
-
--- go next hunk
--- go prev hunk
--- resest hunk gr
---
