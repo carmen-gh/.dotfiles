d9c249f3f6ffceb255a41370171a0b7c13e51103
camina <camina.apps@gmail.com>
2024-01-09 11:23:26 +0100

    add function to toggle checkmark in markdown files for nvim

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -13,3 +13,26 @@ vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
 vim.g.markdown_fenced_languages = { "html", "kotlin", "go", "rust", "dart" } -- enable code highlight in markdown code snippets
+
+-- toggle checkbox
+function ToggleCheckbox()
+	local currentBuffer = vim.api.nvim_get_current_buf()
+	local currentLine = vim.api.nvim_win_get_cursor(0)[1]
+	local lineContent = vim.api.nvim_buf_get_lines(currentBuffer, currentLine - 1, currentLine, false)[1]
+
+	-- Toggle the checkbox in the current line
+	if lineContent:find("%[x%]") then
+		lineContent = lineContent:gsub("%[x%]", "[ ]")
+	elseif lineContent:find("%[ %]") then
+		lineContent = lineContent:gsub("%[ %]", "[x]")
+	end
+
+	vim.api.nvim_buf_set_lines(currentBuffer, currentLine - 1, currentLine, false, { lineContent })
+end
+
+vim.keymap.set(
+	"n",
+	"<leader>ct",
+	":lua ToggleCheckbox()<CR>",
+	{ noremap = true, silent = true, desc = "toggle checkbox" }
+)
