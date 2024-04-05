d4313942cbd55523fe9c6267c7d28978802ce0b6
camina <camina.apps@gmail.com>
2024-04-05 13:25:42 +0200

    cleanup nvim init

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -13,7 +13,6 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = " "
 
 require("lazy").setup("plugins", {
   change_detection = {
