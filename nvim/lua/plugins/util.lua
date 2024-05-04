f2ea7338bd26285d52e6c9ac37227ac2d7662675
Carmen Geelhaar <camina.apps@gmail.com>
2024-05-04 07:51:15 +0200

    move terminal plugin into own file

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -82,33 +82,4 @@ return {
       })
     end,
   },
-  {
-    "letieu/wezterm-move.nvim",
-    keys = { -- Lazy loading, don't need call setup() function
-      {
-        "<C-h>",
-        function()
-          require("wezterm-move").move("h")
-        end,
-      },
-      {
-        "<C-j>",
-        function()
-          require("wezterm-move").move("j")
-        end,
-      },
-      {
-        "<C-k>",
-        function()
-          require("wezterm-move").move("k")
-        end,
-      },
-      {
-        "<C-l>",
-        function()
-          require("wezterm-move").move("l")
-        end,
-      },
-    },
-  },
 }
