c5778ae4a42e6baa6099a7195b78543aac603078
camina <camina.apps@gmail.com>
2024-05-25 11:59:37 +0200

    neovim add better git status icons to file-tree

--- .config/nvim/lua/plugins/file-explorer.lua
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -16,7 +16,16 @@ return {
         },
         default_component_configs = {
           indent = {
-            with_markers = false,
+            with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+          },
+          git_status = {
+            symbols = {
+              unstaged = "󰄱",
+              staged = "󰱒",
+            },
           },
         },
         event_handlers = {
