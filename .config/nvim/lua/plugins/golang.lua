0481cd7eba91ac016d54a92a9995bfcb2a5cf05e
Carmen Geelhaar <camina.apps@gmail.com>
2024-07-06 14:52:59 +0200

    neovim only attach go keymaps on gopls

--- .config/nvim/lua/plugins/golang.lua
+++ .config/nvim/lua/plugins/golang.lua
@@ -18,10 +18,6 @@ return {
         },
       })
 
-      vim.keymap.set("n", "<leader>ce", "<cmd>GoIfErr<cr>", { desc = "if error" })
-      vim.keymap.set("n", "<leader>cj", "<cmd>GoTagAdd json<cr>", { desc = "json tag" })
-      vim.keymap.set("n", "<leader>cg", "<cmd>GoTestsAll<cr>", { desc = "generate tests for file" })
-
       -- NOTE: https://github.com/golang/go/issues/54531#issuecomment-1464982242
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("GoLspAttach", {}),
@@ -29,12 +25,17 @@ return {
         callback = function(event)
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client.name == "gopls" and not client.server_capabilities.semanticTokensProvider then
+            -- only for gopls lsp client
+
             local semantic = client.config.capabilities.textDocument.semanticTokens
             client.server_capabilities.semanticTokensProvider = {
               full = true,
               legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes },
               range = true,
             }
+            vim.keymap.set("n", "<leader>ce", "<cmd>GoIfErr<cr>", { desc = "if error" })
+            vim.keymap.set("n", "<leader>cj", "<cmd>GoTagAdd json<cr>", { desc = "json tag" })
+            vim.keymap.set("n", "<leader>cg", "<cmd>GoTestsAll<cr>", { desc = "generate tests for file" })
           end
         end,
       })
