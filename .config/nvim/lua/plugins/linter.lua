d0e4f420ebd8ac3c5f7aaf4cb7199afaa0687e79
Carmen Geelhaar <camina.apps@gmail.com>
2024-09-12 11:47:24 +0200

    neovim disable markdown lint

--- .config/nvim/lua/plugins/linter.lua
+++ .config/nvim/lua/plugins/linter.lua
@@ -9,6 +9,7 @@ return {
       kotlin = { "ktlint" }, -- detekt not supported
       markdown = { "markdownlint" },
       go = { "golangcilint" },
+      -- markdown = { "markdownlint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
