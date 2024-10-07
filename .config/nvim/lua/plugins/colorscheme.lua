769887a4fe54d57a6ee91221bde02e11e623b40a
Carmen Geelhaar <camina.apps@gmail.com>
2024-10-07 20:34:39 +0200

    neovim add new colorscheme to try out

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
--- -- Catppuccin Theme
+-- Catppuccin
 return {
   "catppuccin/nvim",
   name = "catppuccin",
@@ -9,10 +9,10 @@ return {
     background = { -- :h background
       dark = "mocha",
     },
-    transparent_background = true,
     integrations = {
       alpha = true,
       cmp = true,
+      dadbod_ui = true,
       dap = true,
       dap_ui = true,
       fidget = true,
@@ -71,7 +71,17 @@ return {
   end,
 }
 
--- One Nord Theme
+-- Tokyonight
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd("colorscheme tokyonight-moon")
+--   end,
+-- }
+
+-- One Nord
 -- ( rainbow delimiters not supported)
 -- return {
 --   "rmehri01/onenord.nvim",
@@ -89,5 +99,48 @@ return {
 --   priority = 1000,
 --   config = function()
 --     vim.cmd("colorscheme everforest")
+--   end,
+-- }
+
+-- Kanagawa
+-- ( rainbow delimiters not supported)
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Default options:
+--     require("kanagawa").setup({
+--       compile = false, -- enable compiling the colorscheme
+--       undercurl = true, -- enable undercurls
+--       commentStyle = { italic = true },
+--       functionStyle = {},
+--       keywordStyle = { italic = true },
+--       statementStyle = { bold = true },
+--       typeStyle = {},
+--       transparent = false, -- do not set background color
+--       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+--       terminalColors = true, -- define vim.g.terminal_color_{0,17}
+--       colors = { -- add/modify theme and palette colors
+--         palette = {},
+--         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+--       },
+--       overrides = function(colors) -- add/modify highlights
+--         local theme = colors.theme
+--         return {
+--           Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+--           PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+--           PmenuSbar = { bg = theme.ui.bg_m1 },
+--           PmenuThumb = { bg = theme.ui.bg_p2 },
+--         }
+--       end,
+--       theme = "wave", -- Load "wave" theme when 'background' option is not set
+--       background = { -- map the value of 'background' option to a theme
+--         dark = "wave", -- try "dragon" !
+--         light = "lotus",
+--       },
+--     })
+--
+--     vim.cmd("colorscheme kanagawa")
 --   end,
 -- }
