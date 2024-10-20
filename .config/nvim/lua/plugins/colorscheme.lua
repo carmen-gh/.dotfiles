3d641a6ff504656e39c3e0b5ebb2bdad0a8c8fd8
Carmen Geelhaar <camina.apps@gmail.com>
2024-10-20 16:28:35 +0200

    neovim update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,68 +1,74 @@
--- Catppuccin
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    flavour = "mocha", -- "latte, frappe, macchiato, mocha"
-    background = { -- :h background
-      dark = "mocha",
-    },
-    integrations = {
-      alpha = true,
-      cmp = true,
-      dadbod_ui = true,
-      dap = true,
-      dap_ui = true,
-      fidget = true,
-      gitsigns = true,
-      harpoon = true,
-      markdown = true,
-      mason = true,
-      mini = {
-        enabled = true,
-        indentscope_color = "surface2",
+  -- Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- "latte, frappe, macchiato, mocha"
+      background = { -- :h background
+        dark = "mocha",
       },
-      neotree = true,
-      neotest = true,
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
+      integrations = {
+        alpha = true,
+        cmp = true,
+        dadbod_ui = true,
+        dap = true,
+        dap_ui = true,
+        fidget = true,
+        gitsigns = true,
+        harpoon = true,
+        markdown = true,
+        mason = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "surface2",
         },
-        inlay_hints = {
-          background = true,
+        neotree = true,
+        neotest = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+          inlay_hints = {
+            background = true,
+          },
         },
+        notify = true,
+        treesitter = true,
+        treesitter_context = true,
+        rainbow_delimiters = true,
+        semantic_tokens = true,
+        telescope = {
+          enabled = true,
+        },
+        which_key = true,
       },
-      notify = true,
-      treesitter = true,
-      treesitter_context = true,
-      rainbow_delimiters = true,
-      semantic_tokens = true,
-      telescope = {
-        enabled = true,
-      },
-      which_key = true,
+      custom_highlights = function(colors)
+        return {
+          Pmenu = { bg = colors.surface0 },
+          PmenuSel = { bg = colors.blue, fg = colors.base },
+          NormalFloat = { bg = colors.surface0 },
+          FloatBorder = { bg = colors.surface0 },
+          TelescopeNormal = { bg = colors.surface0 },
+          TelescopePromptPrefix = { fg = colors.blue },
+          TelescopeSelection = { bg = colors.surface1, fg = colors.text },
+          TelescopeMatching = { fg = colors.blue },
+          TelescopeSelectionCaret = { bg = colors.surface1, fg = colors.blue },
+          GitSignsChange = { fg = colors.blue },
+          LazyGitFloat = { bg = colors.mantle },
+          LazyGitBorder = { fg = colors.mantle },
+        }
+      end,
     },
-    custom_highlights = function(colors)
-      return {
-        Pmenu = { bg = colors.surface0 },
-        PmenuSel = { bg = colors.blue, fg = colors.base },
-        NormalFloat = { bg = colors.surface0 },
-        FloatBorder = { bg = colors.surface0 },
-        TelescopeNormal = { bg = colors.surface0 },
-        TelescopePromptPrefix = { fg = colors.blue },
-        TelescopeSelection = { bg = colors.surface1, fg = colors.text },
-        TelescopeMatching = { fg = colors.blue },
-        TelescopeSelectionCaret = { fg = colors.blue },
-        GitSignsChange = { fg = colors.blue },
-        LazyGitFloat = { bg = colors.mantle },
-        LazyGitBorder = { fg = colors.mantle },
-      }
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd("colorscheme catppuccin")
     end,
   },
   config = function(_, opts)
