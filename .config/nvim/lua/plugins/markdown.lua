7f8f7ab5647df26e5fafb8cbb91f483fefb836c0
Carmen Geelhaar <camina.apps@gmail.com>
2025-09-26 22:46:07 +0200

    neovim disable markdown rendering in insert mode

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,43 +1,5 @@
 return {
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-      vim.fn["mkdp#util#install"]()
-    end,
-    keys = {
-      {
-        "<leader>cp",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<cr>",
-        desc = "Markdown Preview",
-      },
-    },
-    -- config = function()
-    --   vim.cmd([[do FileType]])
-    -- end,
-    ft = "markdown, puml",
-    init = function()
-      local g = vim.g
-      g.mkdp_auto_start = 0
-      g.mkdp_auto_close = 1
-      g.mkdp_refresh_slow = 0
-      g.mkdp_command_for_global = 0
-      g.mkdp_open_to_the_world = 0
-      g.mkdp_open_ip = ""
-      g.mkdp_browser = "chrome"
-      g.mkdp_echo_preview_url = 0
-      g.mkdp_browserfunc = ""
-      g.mkdp_theme = "dark"
-      g.mkdp_filetypes = { "markdown", "puml" }
-      g.mkdp_preview_options = {
-        disable_sync_scroll = 0,
-        disable_filename = 1,
-      }
-    end,
-  },
-  {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       code = {
@@ -49,24 +11,8 @@ return {
         sign = false,
         icons = {},
       },
+      render_modes = { "n", "c", "t" },
     },
     ft = { "markdown", "norg", "rmd", "org" },
-    config = function(_, opts)
-      require("render-markdown").setup(opts)
-      Snacks.toggle({
-        name = "Render Markdown",
-        get = function()
-          return require("render-markdown.state").enabled
-        end,
-        set = function(enabled)
-          local m = require("render-markdown")
-          if enabled then
-            m.enable()
-          else
-            m.disable()
-          end
-        end,
-      }):map("<leader>um")
-    end,
   },
 }
