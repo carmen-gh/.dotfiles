5faddf7a89c07c71a21de9103f37648b7424f669
Carmen Geelhaar <carmen.geelhaar@gmail.com>
2026-04-24 23:31:19 +0200

    update neovim config

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -63,7 +63,7 @@ return {
   opts = function()
     return {
       options = {
-        theme = "catppuccin",
+        theme = "catppuccin-nvim",
         component_separators = "",
         section_separators = { left = "", right = "" },
         globalstatus = true,
